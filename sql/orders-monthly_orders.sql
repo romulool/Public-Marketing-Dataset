c87385919542750b990930e90bec13cfae0ab034
Romulo Leite <36579128+romulool@users.noreply.github.com>
2025-05-26 22:02:06 -0300

    Update orders-monthly_orders.sql
    Just tidying up the code

--- sql/orders-monthly_orders.sql
+++ sql/orders-monthly_orders.sql
@@ -1,5 +1,5 @@
 SELECT 	SUBSTR(DATE(order_purchase_timestamp, 'start of month'),1,7) 	AS purchase_month,
-		COUNT(DISTINCT(order_id)) 										AS orders
+	COUNT(DISTINCT(order_id))					AS orders
 FROM olist_orders_dataset
 WHERE order_status != 'canceled'
 GROUP BY 1
