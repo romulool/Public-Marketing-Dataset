ea7a23446f33c3c1544de60772498a66cbe21e2e
Romulo <romulo.leite@purplemetrics.com.br>
2025-05-27 07:47:54 -0300

    Making a better query for monthly orders

---
+++ sql/monthly_orders.sql
@@ -0,0 +1,6 @@
+SELECT STRFTIME('%Y-%m',order_purchase_timestamp) 	AS purchase_month,
+COUNT(DISTINCT(order_id))							AS orders
+FROM olist_orders_dataset
+WHERE order_status != 'canceled'
+GROUP BY 1
+ORDER BY 1;
